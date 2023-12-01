7d288b30dd712943a8c2702057f6f7c9ff5b6baa
Younjae0322 <cjstk491@naver.com>
2023-12-01 15:05:56 +0900

    이미지 파일 업로드 경로 수정

--- src/main/java/com/javalab/boot/config/WebMvcConfiguration.java
+++ src/main/java/com/javalab/boot/config/WebMvcConfiguration.java
@@ -1,14 +1,25 @@
 package com.javalab.boot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
+    /*@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("file:///D:/iworks/jpa/minishoppingmall/files/");*/
+
+    @Value("${com.javalab.boot.upload.path}")
+    private String fileUploadDir;
+
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/files/**")
-                .addResourceLocations("file:///files/");
+                .addResourceLocations("file:" + fileUploadDir + "/");
     }
 }
+
