3168017d0850309de19b652a10cce2195996d1ef
Younjae0322 <cjstk491@naver.com>
2023-12-08 12:13:26 +0900

    none/main itemview 수정

--- src/main/java/com/javalab/boot/config/WebMvcConfiguration.java
+++ src/main/java/com/javalab/boot/config/WebMvcConfiguration.java
@@ -14,13 +14,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
             registry.addResourceHandler("/files/**")
                     .addResourceLocations("file:///D:/utility/iworks/minishoppingmall/files/");
         }*/
-    @Override
+/*    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 학원
         // 로컬 버전
         registry.addResourceHandler("/files/**")
                 .addResourceLocations("file:///D:/iworks/jpa/minishoppingmall/files/");
-    }
+    }*/
 
 /*    // 클라우드 타입 버전
     @Override
@@ -28,5 +28,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         registry.addResourceHandler("/files/**")
                 .addResourceLocations("file:///app/files/");
     }*/
+    // 클라우드 타입 버전
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("file:///home/ec2-user/rdstest/minishoppingmall_aws/build/libs/files/");
+    }
 }
 
